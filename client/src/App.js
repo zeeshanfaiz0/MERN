c151656afb15544bf02cb4a02f970e00ca996ca2
Zeeshan Faiz <zeshan.faiz@ebitlogix.com>
2022-02-14 17:33:59 +0500

    CRD Functions Completed

--- client/src/App.js
+++ client/src/App.js
@@ -1,46 +1,80 @@
-import { useState } from 'react';
-import Axios from 'axios';
-import './App.css';
+import { useState, useEffect } from "react";
+import Axios from "axios";
+import "./App.css";
 
 function App() {
-  const [name,setName]= useState("");
-  const [email,setEmail]= useState("");
-  const [age,setAge]= useState(1);
-  // useEffect(() => {
-  //   Axios.get("http://localhost:3001/getUsers").then((response) => {
-  //     setListOfUsers(response.data);
-  //   });
-  // }, []);
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [age, setAge] = useState(1);
+  const [listOfUsers, setListOfUsers] = useState([]);
+  useEffect(() => {
+    Axios.get("http://localhost:3001/readUsers").then((response) => {
+      setListOfUsers(response.data);
+    });
+  }, []);
 
   const createUser = () => {
     Axios.post("http://localhost:3001/createUser", {
-      userName:name,
-      age:age,
-      email:email
-     });
-     //.then((response) => {
-    //   setListOfUsers([
-    //     ...listOfUsers,
-    //     {
-    //       name,
-    //       age,
-    //       username,
-    //     },
-    //   ]);
-    // });
+      userName: name,
+      age: age,
+      email: email,
+    });
   };
 
+  const updateUser = (id)=>{
+    Axios.put(`http://localhost:3001/updateUser/${id}`, {
+      userName: "Waniya",
+      age: 6,
+      email: "waniya@gmail.com",
+    });
+  }
+  const deleteUser = (id)=>{
+    Axios.delete(`http://localhost:3001/delete/${id}`)
+  }
   return (
     <div className="App">
-     <h1>CRUD App with MongoDb</h1>
+      <h1>CRUD App with MongoDb</h1>
+      <label>UserName</label>
+      <input
+        type="text"
+        required
+        onChange={(event) => {
+          setName(event.target.value);
+        }}
+      ></input>
+      <label>Email</label>
+      <input
+        type="email"
+        required
+        onChange={(event) => {
+          setEmail(event.target.value);
+        }}
+      ></input>
+      <label>Age</label>
+      <input
+        type="number"
+        onChange={(event) => {
+          setAge(event.target.value);
+        }}
+      ></input>
+      <button onClick={createUser}>Add to Users</button>
 
-     <label>UserName</label>
-     <input type="text" required onChange={(event)=>{setName(event.target.value)}}></input>
-     <label>Email</label>
-     <input type="email" required onChange={(event)=>{setEmail(event.target.value)}}></input>
-     <label>Age</label>
-     <input type="number" onChange={(event)=>{setAge(event.target.value)}}></input>
-     <button onClick={createUser}>Add to Users</button>
+      <div>
+        {listOfUsers.map((user) => {
+          return (
+            <div className="userDisplay">
+              <label>User Name :</label>
+              <h1> {user.userName}</h1>
+              <label>Email :</label>
+              <h1> {user.email}</h1>
+              <label>Age :</label>
+              <h1>{user.age}</h1>
+              <button onClick={()=>{updateUser(user._id)}}>Update</button>
+              <button onClick={()=>{deleteUser(user._id)}}>Delete</button>
+            </div>
+          );
+        })}
+      </div>
     </div>
   );
 }
--- server/index.js
+++ server/index.js
@@ -1,28 +1,55 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const cors = require('cors')
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
 const app = express();
 
-const UserModel = require("./models/User")
+const UserModel = require("./models/User");
+
 const PORT = 3001;
 app.use(cors());
 app.use(express.json());
-app.post('/createUser',async(req,res)=>{
-    const { userName, age, email} = req.body;
-    const user = new UserModel({userName:userName, age:age, email:email})
-    try {
-        await user.save();
-        res.send("Data Added Successfully");
-    } catch (error) {
-        console.log(error);
-        res.send("Data not Added Successfully");
+app.get("/readUsers", (req, res) => {
+  UserModel.find({}, (err, result) => {
+    if (err) {
+      res.send(err);
     }
+    res.send(result);
+  });
+});
 
-})
+app.post("/createUser", async (req, res) => {
+  const { userName, age, email } = req.body;
+  const user = new UserModel({ userName: userName, age: age, email: email });
+  try {
+    await user.save();
+    res.send("Data Added Successfully");
+  } catch (error) {
+    console.log(error);
+    res.send("Data not Added Successfully");
+  }
+});
+
+app.put("/updateUser/:id", async (req, res) => {
+  const {id} = req.params;
+  const { userName, age, email } = req.body;
+  try {
+      await UserModel.updateOne({_id:id},{ userName: userName, age: age, email: email })
+    res.send("Data Updated Successfully");
+  } catch (error) {
+    console.log(error);
+    res.send("Data not Updated ");
+  }
+});
 
 
-mongoose.connect("mongodb+srv://zeshanfa:12911234@crud.fsxck.mongodb.net/crudOps?retryWrites=true&w=majority", {
-    useNewUrlParser: true
-})
+app.delete("/delete/:id", async (req, res) => {
+  const { id } = req.params;
+  await UserModel.findByIdAndRemove(id).exec();
+  res.send("User Deleted");
+});
 
-app.listen(PORT,() => console.log(`Server is running on post: http://localhost:${PORT}`));
+mongoose.connect("mongodb://localhost:27017/BlogApp",{ useNewUrlParser: true });
+
+app.listen(PORT, () =>
+  console.log(`Server is running on post: http://localhost:${PORT}`)
+);
